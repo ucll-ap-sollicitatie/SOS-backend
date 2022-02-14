08c126841ff8924f28003868c399ccd40c96fba7
Szymon Nidecki <szymon.nidecki@student.ucll.be>
2022-02-14 12:02:11 +0100

    [API] Converted to JSON

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
 app.get('/', (req, res) => {
-    res.send('Slim op sollicitatie')
+    res.json({message: "Slim op sollicitatie"})
 })
 
 app.listen(port, () => console.log('Currently listening on port ' + port))
