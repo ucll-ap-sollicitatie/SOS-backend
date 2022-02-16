2b1815213e99c69a97005aa6393ac50fafde0fa0
lorenzo3117 <lorenzocatalano37@gmail.com>
2022-02-16 22:19:06 +0100

    Added Roles and Formations

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const usersService = require('./services/users')
-const questionsService = require('./services/questions')
-const questionCategoriesService = require('./services/question_categories')
-const commentsService = require('./services/comments')
-const videosService = require('./services/videos')
+const express = require("express");
+const usersService = require("./services/users");
+const questionsService = require("./services/questions");
+const questionCategoriesService = require("./services/question_categories");
+const commentsService = require("./services/comments");
+const videosService = require("./services/videos");
+const formationsService = require("./services/formations");
+const rolesService = require("./services/roles");
 const app = express();
 const port = 3001;
 
@@ -26,41 +28,66 @@ app.use(
   }
 );
 
-app.get('/', (req, res) => { res.json({message: "Slim op sollicitatie"}) })
+app.get("/", (req, res) => {
+  res.json({ message: "Slim op sollicitatie" });
+});
 
 // CRUD routes for users
-app.get('/users', usersService.getAllUsers)
-app.get('/users/:r_u_number', usersService.getUserById)
-app.post('/users', usersService.createUser)
-app.put('/users/:r_u_number', usersService.updateUser)
-app.delete('/users/:r_u_number', usersService.deleteUser)
+app.get("/users", usersService.getAllUsers);
+app.get("/users/:r_u_number", usersService.getUserById);
+app.post("/users", usersService.createUser);
+app.put("/users/:r_u_number", usersService.updateUser);
+app.delete("/users/:r_u_number", usersService.deleteUser);
 
 // CRUD routes for questions
-app.get('/questions', questionsService.getAllQuestions)
-app.get('/questions-by-category/:question_category_id', questionsService.getAllQuestionsByQuestionCategoryId)
-app.get('/questions/:question_id', questionsService.getQuestionById)
-app.post('/questions', questionsService.createQuestion)
-app.put('/questions/:question_id', questionsService.updateQuestion)
-app.delete('/questions/:question_id', questionsService.deleteQuestion)
+app.get("/questions", questionsService.getAllQuestions);
+app.get(
+  "/questions-by-category/:question_category_id",
+  questionsService.getAllQuestionsByQuestionCategoryId
+);
+app.get("/questions/:question_id", questionsService.getQuestionById);
+app.post("/questions", questionsService.createQuestion);
+app.put("/questions/:question_id", questionsService.updateQuestion);
+app.delete("/questions/:question_id", questionsService.deleteQuestion);
 
 // CRUD routes for question categories
-app.get('/question-categories', questionCategoriesService.getAllQuestionCategories)
-app.get('/question-categories/:question_category_id', questionCategoriesService.getQuestionCategoriesById)
-app.get('/question-categories/:category', questionCategoriesService.getQuestionCategoriesByName)
-app.post('/question-categories', questionCategoriesService.createQuestionCategory)
+app.get(
+  "/question-categories",
+  questionCategoriesService.getAllQuestionCategories
+);
+app.get(
+  "/question-categories/:question_category_id",
+  questionCategoriesService.getQuestionCategoriesById
+);
+app.get(
+  "/question-categories/:category",
+  questionCategoriesService.getQuestionCategoriesByName
+);
+app.post(
+  "/question-categories",
+  questionCategoriesService.createQuestionCategory
+);
 
 // CRUD routes for comments
-app.get('/comments', commentsService.getAllComments)
-app.get('/comments/:comment_id', commentsService.getCommentById)
-app.post('/comments', commentsService.createComment)
-app.put('/comments/:comment_id', commentsService.updateComment)
-app.delete('/comments/:comment_id', commentsService.deleteComment)
+app.get("/comments", commentsService.getAllComments);
+app.get("/comments/:comment_id", commentsService.getCommentById);
+app.post("/comments", commentsService.createComment);
+app.put("/comments/:comment_id", commentsService.updateComment);
+app.delete("/comments/:comment_id", commentsService.deleteComment);
 
 // CRUD routes for videos
-app.get('/videos', videosService.getAllVideos)
-app.get('/videos/:video_id', videosService.getVideoById)
-app.post('/videos', videosService.createVideo)
-app.put('/videos/:video_id', videosService.updateVideo)
-app.delete('/videos/:video_id', videosService.deleteVideo)
+app.get("/videos", videosService.getAllVideos);
+app.get("/videos/:video_id", videosService.getVideoById);
+app.post("/videos", videosService.createVideo);
+app.put("/videos/:video_id", videosService.updateVideo);
+app.delete("/videos/:video_id", videosService.deleteVideo);
 
-app.listen(port, () => console.log(`Currently listening on port ${port}`))
+// CRUD routes for roles
+app.get("/roles", rolesService.getAllRoles);
+
+// CRUD routes for formations
+app.get("/formations", formationsService.getAllFormations);
+app.get("/formations/:formation_id", formationsService.getFormationById);
+app.get("/formations-by-name/:formation", formationsService.getFormationByName);
+
+app.listen(port, () => console.log(`Currently listening on port ${port}`));
--- services/formations.js
+++ services/formations.js
@@ -1,28 +1,45 @@
 // Contains all the queries for the table 'formations'
-const db = require('../configuration/db')
+const db = require("../configuration/db");
 
 const getAllFormations = (req, res) => {
-    console.log(`Request for all formations`)
-    db.query('SELECT * FROM formations ORDER BY formation_id ASC', (err, results) => {
-        if (err) throw err
-        res.status(200).json(results.rows)
-    })
-}
+  console.log(`Request for all formations`);
+  db.query(
+    "SELECT * FROM formations ORDER BY formation_id ASC",
+    (err, results) => {
+      if (err) throw err;
+      res.status(200).json(results.rows);
+    }
+  );
+};
 
 const getFormationById = (req, res) => {
-    const formation_id = req.params.formation_id
-    console.log(`Request for formation by id with id #${formation_id}`)
-    db.query('SELECT * FROM formations WHERE formation_id = $1', [formation_id], (err, results) => {
-        if (err) throw err
-        res.status(200).json(results.rows)
-    })
-}
+  const formation_id = req.params.formation_id;
+  console.log(`Request for formation by id with id #${formation_id}`);
+  db.query(
+    "SELECT * FROM formations WHERE formation_id = $1",
+    [formation_id],
+    (err, results) => {
+      if (err) throw err;
+      res.status(200).json(results.rows);
+    }
+  );
+};
 
 const getFormationByName = (req, res) => {
-    const formation_name = req.params.formation_name
-    console.log(`Request for formation by name with name: ${formation_name}`)
-    db.query('SELECT * FROM formations WHERE formation = $1', [formation_name], (err, results) => {
-        if (err) throw err
-        res.status(200).json(results.rows)
-    })
-}
+  const formation_name = req.params.formation_name;
+  console.log(`Request for formation by name with name: ${formation_name}`);
+  db.query(
+    "SELECT * FROM formations WHERE formation = $1",
+    [formation_name],
+    (err, results) => {
+      if (err) throw err;
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+module.exports = {
+  getAllFormations,
+  getFormationById,
+  getFormationByName,
+};
