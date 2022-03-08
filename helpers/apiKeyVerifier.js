e742d41028d38fc344e3f8b7a5fa3df7061d196e
ghostek99 <szymon.nidecki@student.ucll.be>
2022-03-08 10:36:01 +0100

    api key protection test

---
+++ helpers/apiKeyVerifier.js
@@ -0,0 +1,22 @@
+const crypto = require("crypto");
+
+const verifyApiKey = (req, res, next) => {
+  const givenApiKey = req.headers["X-API-KEY"];
+  console.log(req.hostname);
+  if (!givenApiKey) {
+    res.status(401).send("Please provide a correct 'x-api-key' header to gain access.");
+    return;
+  }
+
+  const hash = crypto.createHash("sha512");
+
+  if (crypto.timingSafeEqual(hash.copy().update(givenApiKey).digest(), hash.copy().update(process.env.API_KEY).digest())) {
+    next();
+  } else {
+    res.status(401).send("Unauthorized api-key.");
+  }
+};
+
+module.exports = {
+  verifyApiKey,
+};
