43d5b0d584b6805fb10ebba28ce4b5fe67bea7d8
ghostek99 <szymon.nidecki@student.ucll.be>
2022-03-08 10:52:08 +0100

    hotfix api key

--- helpers/apiKeyVerifier.js
+++ helpers/apiKeyVerifier.js
@@ -2,15 +2,18 @@ require("dotenv").config();
 const crypto = require("crypto");
 
 const verifyApiKey = (req, res, next) => {
-  const givenApiKey = req.headers["X-API-KEY"];
+  const givenApiKey = req.headers["x-api-key"];
+
   if (req.hostname === "api.szymon-nidecki.sb.uclllabs.be") {
     next();
     return;
   }
+
   if (!givenApiKey) {
     res.status(401).send("Please provide a correct 'x-api-key' header to gain access.");
     return;
   }
+
   const hash = crypto.createHash("sha512");
   if (crypto.timingSafeEqual(hash.copy().update(givenApiKey).digest(), hash.copy().update(process.env.API_KEY).digest())) {
     next();
